1fbdbe58113a6ec404f9146bf910c8cc8a3374f6
zrsoo <46485193+zrsoo@users.noreply.github.com>
2023-05-12 15:53:11 +0300

    Initial

---
+++ Assets/Flame/FlameMovementController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO Adjust flame flicker speed in ordinance with flame movement speed
+
+public class FlameMovementController : MonoBehaviour
+{
+    private RaycastHit hitPlace;
+    private RaycastHit hitMove;
+
+    private bool flameOnTable;
+
+    private Vector3 movementDirection;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.05f;
+
+        PlaceFlameOnTable();
+        GenerateRandomMovementDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        
+        // Cast ray straight down (while looking ahead, in order to change course before going off the table).
+        if(Physics.Raycast(transform.position + movementDirection * (speed + 1.7f) * Time.deltaTime, -Vector3.up, out hitMove))
+        {
+            // If it hits the table.
+            if(hitMove.collider.gameObject.tag == "Table")
+            {
+                flameOnTable = true;
+            }
+            else
+            {
+                flameOnTable = false;
+            }
+        }
+
+        // If the flame is not on the table, pick another random direction.
+        if(!flameOnTable)
+        {
+            GenerateRandomMovementDirection();
+            flameOnTable = true;
+        }
+    }
+
+    void PlaceFlameOnTable()
+    {
+        // Cast a ray straight down.
+        if (Physics.Raycast(transform.position, -Vector3.up, out hitPlace))
+        {
+            // If it hits the table.
+            if (hitPlace.collider.gameObject.tag == "Table")
+            {
+                // Position flame on table (slightly above).
+                transform.position = hitPlace.point + new Vector3(0, 0.1f, 0);
+            }
+        }
+    }
+
+    void GenerateRandomMovementDirection()
+    {
+        GameObject table = GameObject.FindGameObjectWithTag("Table");
+        Vector3 tableCenterPosition = new Vector3(table.transform.position.x, transform.position.y, table.transform.position.z);
+        Vector3 directionToTableCenter = (tableCenterPosition - transform.position).normalized;
+
+        // Create a random offset.
+        float angleOffset = Random.Range(-45.0f, 45.0f); // Change this range depending on how much randomness you want
+        Quaternion rotation = Quaternion.Euler(0, angleOffset, 0);
+
+        // Apply the random offset to the direction.
+        movementDirection = rotation * directionToTableCenter;
+    }
+}
---
+++ Assets/Flame/ProceduralFlame.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class ProceduralFlame : MonoBehaviour
+{
+    private Mesh mesh;
+    private MeshRenderer meshRenderer;
+
+    private float speed = 25.0f;
+    private float timePeriod = 100.0f;
+    private float time = 0.0f;
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        int segments = 16;
+
+        // Create the rhombus shape by specifying vertex positions
+        Vector3[] vertices = new Vector3[segments * 4];
+
+        for (int i = 0; i < segments; i++)
+        {
+            float t = i / (float)(segments - 1);
+            float y = t - 0.5f;
+
+            float x_offset;
+
+            if (t < 1.0f / 3.0f)
+            {
+                x_offset = Mathf.Lerp(0.1f, 0.25f, Mathf.Abs(t));
+            }
+            else
+            {
+                x_offset = Mathf.Lerp(0.25f, 0.0f, Mathf.Abs(t));
+            }
+
+            float angle = Mathf.PI * Mathf.Abs(t);
+            float x_left = 0.5f - x_offset + Mathf.Cos(angle) * 0.07f;
+            float x_right = 0.5f + x_offset + Mathf.Cos(angle) * 0.07f;
+
+            vertices[i] = new Vector3(x_left, y, 0);
+            vertices[segments + i] = new Vector3(x_right, y, 0);
+        }
+
+        // Create the triangles
+        int[] triangles = new int[(segments - 1) * 6];
+        for (int i = 0; i < segments - 1; i++)
+        {
+            int idx = i * 6;
+
+            triangles[idx] = i;
+            triangles[idx + 1] = i + 1;
+            triangles[idx + 2] = segments + i;
+
+            triangles[idx + 3] = i + 1;
+            triangles[idx + 4] = segments + i + 1;
+            triangles[idx + 5] = segments + i;
+        }
+
+        // Assign the vertices and triangles to the mesh
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        // Generate UVs based on vertex positions
+        Vector2[] uvs = new Vector2[vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            uvs[i] = new Vector2(vertices[i].x, vertices[i].y);
+        }
+        mesh.uv = uvs;
+
+        // Calculate normals
+        mesh.RecalculateNormals();
+    }
+
+    private void Update()
+    {
+        time = (Time.time % timePeriod) / timePeriod;
+        time = Mathf.Sin(time * 2 * Mathf.PI) * speed;
+
+        float noisyAmplitude = Mathf.PerlinNoise(time, 0.0f);
+        noisyAmplitude *= 1.3f;
+
+        float noisySpeed = Mathf.PerlinNoise(0.0f, time);
+        noisySpeed *= 0.5f;
+
+        meshRenderer.material.SetFloat("_WaveAmplitude", noisyAmplitude);
+        meshRenderer.material.SetFloat("_WaveSpeed", noisySpeed);
+    }
+}
